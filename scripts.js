cef2c0f3daf847ea1bceeda99bece39084ad6e86
staffordgavin19 <staffordgavin19@gmail.com>
2021-04-27 22:49:19 -0500

    Add pricing data

--- scripts.js
+++ scripts.js
@@ -1,1 +1,45 @@
+const ul = document.body.appendChild(document.createElement('ul'));
+
+const sliderBar = document.querySelector('#sliderBar');
+
+// I assume this is the data you wanted us to use from repl.it
+const pricesArr [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball"
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch"
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5"
+  },
+  {
+    category: "Electronics",
+    price: "$199.99",
+    stocked: true,
+    name: "Nexus 7"
+  }
+];
+
 
