975c9e9df1bba69611e11c8c584478e4b09ad986
staffordgavin19 <staffordgavin19@gmail.com>
2021-04-27 23:06:05 -0500

    Add event listener

--- scripts.js
+++ scripts.js
@@ -3,50 +3,60 @@ const ul = document.body.appendChild(document.createElement('ul'));
 const sliderBar = document.querySelector('#sliderBar');
 
 // I assume this is the data you wanted us to use from repl.it
-const pricesArr [
+const pricesArr = [
   {
-    category: "Sporting Goods",
-    price: "$49.99",
+    category: 'Sporting Goods',
+    price: '$49.99',
     stocked: true,
-    name: "Football"
+    name: 'Football',
   },
   {
-    category: "Sporting Goods",
-    price: "$9.99",
+    category: 'Sporting Goods',
+    price: '$9.99',
     stocked: true,
-    name: "Baseball"
+    name: 'Baseball',
   },
   {
-    category: "Sporting Goods",
-    price: "$29.99",
+    category: 'Sporting Goods',
+    price: '$29.99',
     stocked: false,
-    name: "Basketball"
+    name: 'Basketball',
   },
   {
-    category: "Electronics",
-    price: "$99.99",
+    category: 'Electronics',
+    price: '$99.99',
     stocked: true,
-    name: "iPod Touch"
+    name: 'iPod Touch',
   },
   {
-    category: "Electronics",
-    price: "$399.99",
+    category: 'Electronics',
+    price: '$399.99',
     stocked: false,
-    name: "iPhone 5"
+    name: 'iPhone 5',
   },
   {
-    category: "Electronics",
-    price: "$199.99",
+    category: 'Electronics',
+    price: '$199.99',
     stocked: true,
-    name: "Nexus 7"
-  }
+    name: 'Nexus 7',
+  },
 ];
 
-function renderItems(items,) {
-  ul.innerHTML = items.map(item => `
+function renderItems(items) {
+  ul.innerHTML = items
+    .map(
+      item => `
   <li>
   <>
     id="${item.name}">${item.name} ${item.price}
   </h3>
-  </li>`,).join('');
+  </li>`,
+    )
+    .join('');
 }
+
+sliderBar.addEventListener('input', event => {
+  renderItems(pricesArr, event.target.value);
+});
+
+// Not sure how to implement the maxPrice
